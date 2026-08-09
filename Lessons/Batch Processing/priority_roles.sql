832409242b5bf53a7d5181470801b795e2b66ab1
RickyMm02 <grayyeonmm@gmail.com>
2026-08-09 21:41:50 +0530

    Batch processing sql queries

---
+++ Lessons/Batch Processing/priority_roles.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE TABLE staging.priority_roles (
+    role_id INTEGER PRIMARY KEY,
+    role_name VARCHAR,
+    priority_lvl INTEGER
+);
+
+INSERT INTO staging.priority_roles (role_id, role_name, priority_lvl)
+VALUES
+    (1, 'Data Engineer', 3),
+    (2, 'Senior Data Engineer', 1),
+    (3, 'Data Analyst', 2),
+    (4, 'Software Engineer', 2);
+
+SELECT * from staging.priority_roles;
