117ff4dbde6a84185aac77652551391785c6b4b7
RickyMm02 <grayyeonmm@gmail.com>
2026-09-14 02:39:33 +0530

    adding load and build master file

---
+++ Data_Warehouse/build_dw_marts.sql
@@ -0,0 +1,7 @@
+-- duckdb dw_marts.duckdb -c ".read build_dw_marts.sql"
+
+-- Step 1: DW - Create star schema tables
+.read create_tables_dw.sql
+
+-- Step 2: DW - Load data from CSV files into tables
+.read load_schema_dw.sql
